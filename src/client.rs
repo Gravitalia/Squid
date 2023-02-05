00919d4a366c5b189a5737a2d983a68567bed7fa
Hinome <57831472+RealHinome@users.noreply.github.com>
2023-02-05 20:23:23 +0100

    Add client

---
+++ src/client.rs
@@ -0,0 +1,22 @@
+use std::time::{SystemTime, UNIX_EPOCH};
+use squid::squid_client::SquidClient;
+use squid::SquidRequest;
+
+pub mod squid {
+    tonic::include_proto!("squid");
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let mut client = SquidClient::connect("http://[::1]:50051").await?;
+
+    let request = tonic::Request::new(SquidRequest {
+        message: "This a #Gravitalia test!".into()
+    });
+
+    let start = SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_millis();
+    let response = client.squid_index(request).await?;
+    println!("Response with {:?} in {}ms", response.into_inner().message, SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_millis()-start);
+
+    Ok(())
+}
