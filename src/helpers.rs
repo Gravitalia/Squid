627d059dc6b72da3cfbc5ab4bb9529bdc5bd27cd
Hinome <57831472+RealHinome@users.noreply.github.com>
2023-04-22 23:05:40 +0200

    Add detect language function

--- src/helpers.rs
+++ src/helpers.rs
@@ -1,14 +1,16 @@
+use lingua::{Language, LanguageDetector, LanguageDetectorBuilder};
+use lingua::Language::{English, French, Spanish};
+use anyhow::Result;
 use std::fs;
-use std::io;
 
-// Copy a file from a folder to another one
-pub fn copy_file(src: &str, dst: &str) -> io::Result<()> {
+/// Copy a file from a folder to another one
+pub fn copy_file(src: &str, dst: &str) -> Result<()> {
     fs::copy(src, dst)?;
     Ok(())
 }
 
-// Copy a directory to another one
-pub fn copy_dir(src: &str, dst: &str) -> io::Result<()> {
+/// Copy a directory to another one
+pub fn copy_dir(src: &str, dst: &str) -> Result<()> {
     fs::create_dir_all(dst)?;
 
     for entry in fs::read_dir(src)? {
@@ -25,4 +27,20 @@ pub fn copy_dir(src: &str, dst: &str) -> io::Result<()> {
     }
 
     Ok(())
+}
+
+/// Detect a language between english, french and spanish
+pub fn detect_language(text: String) -> Result<Option<&'static str>> {
+    let detector: LanguageDetector = LanguageDetectorBuilder::from_languages(&vec![English, French, Spanish]).build();
+    let detected_language: Option<Language> = detector.detect_language_of(text);
+
+    if detected_language.is_none() {
+        return Ok(None);
+    }
+
+    match detected_language.unwrap() {
+        English => Ok(Some("en")),
+        French => Ok(Some("fr")),
+        Spanish => Ok(Some("es"))
+    }
 }
