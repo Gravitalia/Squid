b40d455fa9fa10efcb819ec5758f33526eab72e8
Hinome <57831472+RealHinome@users.noreply.github.com>
2024-01-30 18:07:28 +0100

    Delete everything to recreate everything (#29)

--- src/solr/core.rs
+++
@@ -1,9 +0,0 @@
-use anyhow::Result;
-
-pub async fn create(name: String) -> Result<()> {
-    reqwest::Client::new().post(format!("http://localhost:8983/solr/admin/cores?action=CREATE&name={}&configSet={}/solr/configsets/squid", name, super::properties().await?.system_properties.jetty_home))
-        .send()
-        .await?;
-
-    Ok(())
-}
