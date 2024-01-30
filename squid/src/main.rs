cd65599dcb5fc3445287fac17b318ea4df0a0d58
Hinome <57831472+RealHinome@users.noreply.github.com>
2024-01-30 21:30:27 +0100

    feat: start zeromq

--- squid/src/main.rs
+++ squid/src/main.rs
@@ -1,3 +1,42 @@
+mod helpers;
+
 fn main() {
-    println!("Hello, world!");
+    // Set logger with Fern.
+    fern::Dispatch::new()
+        .format(|out, message, record| {
+            out.finish(format_args!(
+                "[{} {}] {}",
+                helpers::format::format_rfc3339(
+                    std::time::SystemTime::now()
+                        .duration_since(std::time::UNIX_EPOCH)
+                        .expect("Time went backwards")
+                        .as_secs()
+                ),
+                record.level(),
+                message
+            ))
+        })
+        .level(if cfg!(debug_assertions) {
+            log::LevelFilter::Trace
+        } else {
+            log::LevelFilter::Info
+        })
+        .chain(std::io::stdout())
+        .apply()
+        .unwrap();
+
+    let port = std::env::var("port").unwrap_or_else(|_| "5555".to_string());
+    let responder = zmq::Context::new().socket(zmq::REP).unwrap();
+
+    match responder.bind(&format!("tcp://*:{}", port)) {
+        Ok(_) => log::info!("Started Squid server on port {}", port),
+        Err(error) => log::error!("Failed starting Squid: {}", error),
+    }
+
+    let mut msg = zmq::Message::new();
+    loop {
+        responder.recv(&mut msg, 0).unwrap();
+        println!("Received {}", msg.as_str().unwrap());
+        responder.send("World", 0).unwrap();
+    }
 }
