f6be466cc47b1b652cb23eaecf0d49263a3a9ec7
Hinome <57831472+RealHinome@users.noreply.github.com>
2024-01-31 22:14:19 +0100

    add tokenizer benchmark

--- benchmarks/benches/tokenize.rs
+++ benchmarks/benches/tokenize.rs
@@ -1,10 +1,11 @@
 use criterion::{criterion_group, criterion_main, Criterion};
+use squid::helpers::tokenizer::tokenize;
 
 fn tokenize_benchmark(c: &mut Criterion) {
-    c.bench_function("tokenize", |b| {
-        // To do.
-        b.iter(|| println!("tokenize"))
-    });
+    const FRENCH: &str =
+        "Le soleil brille, illuminant la ville endormie. Les rues sont calmes, baignées dans une douce lumière. Au loin, les oiseaux oisifs chantent la vie !";
+
+    c.bench_function("tokenize 150 bytes", |b| b.iter(|| tokenize(FRENCH)));
 }
 
 criterion_group!(benches, tokenize_benchmark);
