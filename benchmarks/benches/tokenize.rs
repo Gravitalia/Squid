54d5640d0403ef9e91e807ea99606cbb467214df
Hinome <57831472+RealHinome@users.noreply.github.com>
2024-02-04 21:39:44 +0100

    chore: update tokenizer benchmark

--- benchmarks/benches/tokenize.rs
+++ benchmarks/benches/tokenize.rs
@@ -1,5 +1,5 @@
 use criterion::{criterion_group, criterion_main, Criterion};
-use squid::helpers::tokenizer::tokenize;
+use squid_tokenizer::tokenize;
 
 fn tokenize_benchmark(c: &mut Criterion) {
     const FRENCH: &str =
