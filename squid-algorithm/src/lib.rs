39d4bf5f1a403972710cf3f0b3a94f6aa2f2ec6f
Hinome <57831472+RealHinome@users.noreply.github.com>
2024-02-21 22:33:49 +0100

    doc: add library documentation

--- squid-algorithm/src/lib.rs
+++ squid-algorithm/src/lib.rs
@@ -1,2 +1,11 @@
+//! # squid-algorithm
+//!
+//! crazy algorithms to quickly rank the most frequently used words in a sentence!
+//! Supported algorithms:
+//! - HashMap;
+
+#![forbid(unsafe_code)]
+#![deny(unused_imports, unused_mut, missing_docs)]
+
 /// The most accurate algorithm for ranking.
 mod hashtable;
