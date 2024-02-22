443c0432e0a197600b9417f1e7b0317013fcd0f0
Hinome <57831472+RealHinome@users.noreply.github.com>
2024-02-23 00:00:24 +0100

    doc: better documentation

--- benchmarks/src/lib.rs
+++ benchmarks/src/lib.rs
@@ -1,3 +1,6 @@
 #![deny(missing_docs)]
-//! library used internally for benchmarks.
-//! not used on other projects.
+//! library used internally for benchmarks. not used on other projects.
+//! the code behind is just here to create cool datasets!
+//!
+//! juste execute `cargo bench` and wait until it finishes to
+//! download datasets and run benchmarks.
