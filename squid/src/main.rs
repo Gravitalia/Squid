504f9c827147bc2f9e6187adb873065070795870
Hinome <57831472+RealHinome@users.noreply.github.com>
2024-03-10 18:20:21 +0100

    working set

--- squid/src/main.rs
+++ squid/src/main.rs
@@ -7,7 +7,8 @@ use squid::{
     squid_server::{Squid, SquidServer},
     {AddRequest, LeaderboardRequest, Ranking, Void, Word},
 };
-use std::time::{SystemTime, UNIX_EPOCH};
+use squid_tokenizer::tokenize;
+use std::sync::{Arc, RwLock};
 use tonic::{transport::Server, Request, Response, Status};
 
 pub mod squid {
@@ -16,6 +17,7 @@ pub mod squid {
 
 struct SuperSquid {
     algorithm: helpers::database::Algorithm,
+    instance: Arc<RwLock<squid_db::Instance<models::database::Entity>>>,
 }
 
 #[tonic::async_trait]
@@ -25,10 +27,16 @@ impl Squid for SuperSquid {
         request: Request<LeaderboardRequest>,
     ) -> Result<Response<Ranking>, Status> {
         Ok(Response::new(Ranking {
-            word: Some(Word {
-                word: "test".to_string(),
-                occurence: 0,
-            }),
+            word: helpers::database::rank(
+                self.algorithm.clone(),
+                request.into_inner().length as usize,
+            )
+            .iter()
+            .map(|(word, occurence)| Word {
+                word: word.to_string(),
+                occurence: (*occurence).try_into().unwrap_or_default(),
+            })
+            .collect::<Vec<_>>(),
         }))
     }
 
@@ -36,6 +44,31 @@ impl Squid for SuperSquid {
         &self,
         request: Request<AddRequest>,
     ) -> Result<Response<Void>, Status> {
+        let data = request.into_inner();
+
+        helpers::database::set(
+            Arc::clone(&self.instance),
+            self.algorithm.clone(),
+            models::database::Entity {
+                id: String::default(),
+                original_text: None,
+                post_processing_text: tokenize(&data.sentence).map_err(
+                    |error| {
+                        log::error!(
+                            "Failed to tokenize {:?}: {}",
+                            data.sentence,
+                            error
+                        );
+                        Status::invalid_argument("failed to tokenize sentence")
+                    },
+                )?,
+                lang: "fr".to_string(),
+                ttl: data.lifetime as usize,
+                creation_date: 0,
+            },
+        )
+        .unwrap();
+
         Ok(Response::new(Void {}))
     }
 }
@@ -69,7 +102,7 @@ async fn main() {
     let config = helpers::config::read();
 
     // Start database.
-    let mut db_instance: squid_db::Instance<models::database::Entity> =
+    let db_instance: squid_db::Instance<models::database::Entity> =
         squid_db::Instance::new().unwrap();
     log::info!(
         "Loaded instance with {} entities.",
@@ -85,24 +118,6 @@ async fn main() {
         }
     }
 
-    let start = SystemTime::now()
-        .duration_since(UNIX_EPOCH)
-        .unwrap()
-        .as_micros();
-
-    let rank = algo.rank(3);
-
-    println!("Rank of the 3 most used words: {:?}", rank);
-
-    println!(
-        "Took {}μs",
-        SystemTime::now()
-            .duration_since(UNIX_EPOCH)
-            .unwrap()
-            .as_micros()
-            - start
-    );
-    
     let addr = format!("0.0.0.0:{}", config.port.unwrap_or(50051))
         .parse()
         .unwrap();
@@ -112,6 +127,7 @@ async fn main() {
     Server::builder()
         .add_service(SquidServer::new(SuperSquid {
             algorithm: helpers::database::Algorithm::Map(algo),
+            instance: Arc::new(RwLock::new(db_instance)),
         }))
         .serve(addr)
         .await
