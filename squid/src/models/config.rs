fc529cf143c58be9782dbebf84ec9541e528455a
Hinome <57831472+RealHinome@users.noreply.github.com>
2024-03-18 19:14:57 +0100

    add service

--- squid/src/models/config.rs
+++ squid/src/models/config.rs
@@ -4,4 +4,43 @@ use serde::Deserialize;
 #[derive(Deserialize, Debug)]
 pub struct Config {
     pub port: Option<u16>,
+    pub service: Service,
+}
+
+/// The algorithm used to rank the most frequently used words.
+#[derive(Deserialize, Debug, Default)]
+pub enum Algorithm {
+    #[default]
+    Hashmap,
+}
+
+/// Which words need to be selected to be classified.
+#[derive(Deserialize, Debug, Default)]
+pub enum MessageType {
+    #[default]
+    Anything,
+    Word,
+    Hashtag,
+}
+
+/// Definition of a service. A service is equal to a database.
+#[derive(Deserialize, Debug)]
+#[allow(unused)]
+pub struct Service {
+    /// Name of the database.
+    name: String,
+    /// The algorithm to be used.
+    /// This affects RAM consumption and accuracy.
+    #[serde(default)]
+    pub algorithm: Algorithm,
+    /// The maximum number of words returned for a query.
+    max_words: Option<usize>,
+    /// What data the algorithm needs to cache.
+    #[serde(default)]
+    pub message_type: MessageType,
+    /// The language of words to be returned.
+    lang: Option<String>,
+    /// Words to exclude from the search.
+    #[serde(default)]
+    pub exclude: Vec<String>,
 }
