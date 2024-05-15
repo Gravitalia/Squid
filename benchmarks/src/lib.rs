4765e1c6a430ef3001daa08ea89deae0b8d22a61
Hinome <57831472+RealHinome@users.noreply.github.com>
2024-05-15 11:32:15 +0200

    doc: update command

--- benchmarks/src/lib.rs
+++ benchmarks/src/lib.rs
@@ -2,5 +2,7 @@
 //! library used internally for benchmarks. not used on other projects.
 //! the code behind is just here to create cool datasets!
 //!
-//! juste execute `cargo bench` and wait until it finishes to
-//! download datasets and run benchmarks.
+//! to create the datasets, you need to download:
+//! - https://ai2-public-datasets.s3.amazonaws.com/zest/zest.zip
+//! then extract files into `benchmarks` root.
+//! and after, execute `cargo bench`.
