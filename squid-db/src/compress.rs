5652dce20b1a1495b156ea50168b0d8073e2358b
Hinome <57831472+RealHinome@users.noreply.github.com>
2024-05-15 12:21:15 +0200

    Add files via upload (#83)
    * Add files via upload
    
    * lockfile

--- squid-db/src/compress.rs
+++ squid-db/src/compress.rs
@@ -16,6 +16,7 @@ pub fn compress(mut source: &[u8]) -> Result<Vec<u8>> {
 
 /// Decompress data using `lz4`.
 pub fn decompress(buf: &[u8]) -> Result<Vec<u8>> {
+    //let input_file: File = File::open(SOURCE_FILE)?;
     let mut decoder = Decoder::new(buf)?;
     let mut output = Vec::new();
     io::copy(&mut decoder, &mut output)?;
