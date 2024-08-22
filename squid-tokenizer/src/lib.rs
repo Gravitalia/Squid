d2089eabb7804d86ee464bf1d0e9f2e849ad9064
Hinome <57831472+RealHinome@users.noreply.github.com>
2024-08-22 19:54:35 +0200

    Use Infallible

--- squid-tokenizer/src/lib.rs
+++ squid-tokenizer/src/lib.rs
@@ -1,17 +1,15 @@
 pub mod stopwords;
 
-use anyhow::Result;
-use std::{collections::HashSet, path::Path};
+use std::{collections::HashSet, convert::Infallible, path::Path};
 
 /// Lowercase words, remove punctuation, separate words into tokens and convert them into numbers.
-pub fn tokenize<T: ToString>(text: T) -> Result<String> {
+pub fn tokenize<T: ToString>(text: T) -> Result<String, Infallible> {
     stopwords::init(Path::new("./stopwords").to_path_buf());
 
-    let punctuation: HashSet<char> =
-        ['!', ',', '.', ':', ';', '?', '-', '\"', '(', ')']
-            .iter()
-            .cloned()
-            .collect();
+    let punctuation: HashSet<char> = ['!', ',', '.', ':', ';', '?', '-', '\"', '(', ')']
+        .iter()
+        .cloned()
+        .collect();
 
     let result_string: String = stopwords::remove_words_from_sentence(
         text.to_string()
@@ -46,8 +44,7 @@ mod tests {
 
     #[test]
     fn test_tokenize() {
-        let plaintext =
-            "I really like apples! But I prefer Gravitalia, sometimes... yeah?";
+        let plaintext = "I really like apples! But I prefer Gravitalia, sometimes... yeah?";
 
         assert_eq!(
             tokenize(plaintext).unwrap(),
